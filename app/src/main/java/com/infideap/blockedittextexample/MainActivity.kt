ff93064888f6fd3ac079398a5e713894250fc06c
Shiburagi <tr32010@gmail.com>
2020-10-06 16:27:23 +0800

    convert to kotlin

--- app/src/main/java/com/infideap/blockedittextexample/MainActivity.kt
+++ app/src/main/java/com/infideap/blockedittextexample/MainActivity.kt
@@ -1,66 +1,45 @@
-package com.infideap.blockedittextexample;
+package com.infideap.blockedittextexample
 
-import android.graphics.Color;
-import android.os.Bundle;
-import androidx.core.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.InputType;
-import android.text.TextWatcher;
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import androidx.core.content.ContextCompat
+import com.infideap.blockedittext.BlockEditText
+import com.infideap.blockedittext.CardPrefix
 
-import com.infideap.blockedittext.BlockEditText;
-import com.infideap.blockedittext.CardPrefix;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final BlockEditText amexEditText = findViewById(R.id.blockEditText_amex);
-        amexEditText.setNumberOfBlock(3);
-        amexEditText.setDefaultLength(4);
-        amexEditText.setLengthAt(1, 6);
-        amexEditText.setLengthAt(2, 5);
-        amexEditText.setHint("Amex");
-        amexEditText.setSeparatorCharacter('-');
-        amexEditText.setInputType(InputType.TYPE_CLASS_NUMBER);
-        amexEditText.setEdiTextBackground(ContextCompat.getDrawable(this, com.infideap.blockedittext.R.drawable.selector_edittext_round_border_line));
-
-        amexEditText.setTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val amexEditText: BlockEditText = findViewById(R.id.blockEditText_amex)
+        amexEditText.setNumberOfBlock(3)
+        amexEditText.setDefaultLength(4)
+        amexEditText.setLengthAt(1, 6)
+        amexEditText.setLengthAt(2, 5)
+        amexEditText.setHint("Amex")
+        amexEditText.setSeparatorCharacter('-')
+        amexEditText.setInputType(InputType.TYPE_CLASS_NUMBER)
+        amexEditText.setEdiTextBackground(ContextCompat.getDrawable(this, com.infideap.blockedittext.R.drawable.selector_edittext_round_border_line))
+        amexEditText.setTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.length >= amexEditText.maxLength) {
+                    amexEditText.setBackgroundColor(Color.GREEN)
+                } else amexEditText.setBackgroundColor(Color.TRANSPARENT)
             }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() >= amexEditText.getMaxLength()) {
-                    amexEditText.setBackgroundColor(Color.GREEN);
-                } else
-                    amexEditText.setBackgroundColor(Color.TRANSPARENT);
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-
-        BlockEditText cardEditText = findViewById(R.id.blockEditText_card);
-        cardEditText.addCardPrefix(CardPrefix.amex(this));
-
-        BlockEditText icNumberEditText = findViewById(R.id.blockEditText_ic_number);
-        icNumberEditText.setNumberOfBlock(3);
-        icNumberEditText.setDefaultLength(6);
-        icNumberEditText.setLengthAt(1, 2);
-        icNumberEditText.setLengthAt(2, 4);
-
-        icNumberEditText.setSelection(0);
-
-
+            override fun afterTextChanged(s: Editable) {}
+        })
+        val cardEditText: BlockEditText = findViewById(R.id.blockEditText_card)
+        cardEditText.addCardPrefix(CardPrefix.amex(this))
+        val icNumberEditText: BlockEditText = findViewById(R.id.blockEditText_ic_number)
+        icNumberEditText.setNumberOfBlock(3)
+        icNumberEditText.setDefaultLength(6)
+        icNumberEditText.setLengthAt(1, 2)
+        icNumberEditText.setLengthAt(2, 4)
+        icNumberEditText.setSelection(0)
     }
 }
