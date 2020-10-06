89912a5791d96693640c651d8fda78174dca1a27
Shiburagi <tr32010@gmail.com>
2020-10-06 12:09:24 +0800

    update gitignore

--- app/src/androidTest/java/com/infideap/blockedittextexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/infideap/blockedittextexample/ExampleInstrumentedTest.java
@@ -1,11 +1,12 @@
 package com.infideap.blockedittextexample;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import static org.junit.Assert.*;
 
@@ -14,12 +15,12 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.infideap.blockedittext", appContext.getPackageName());
     }
